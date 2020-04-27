fdeba213a9538f8739b32c35be627c22096e4d4b
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-04-27 23:04:48 +0300

    Add regist user logic

--- src/components/auth/registr/Registr.jsx
+++ src/components/auth/registr/Registr.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import FormInput from '../../form-input/FormInput';
 
+import { auth, createUserDocument } from '../../../firebase/firebase.utils';
 import ButtonCustom from '../../button-custom/ButtonCustom';
+import { Alert } from '../../alert/Alert';
 
 import './Registr.scss';
-import { Alert } from '../../alert/Alert';
 
 class Registr extends Component {
     constructor(props) {
@@ -25,29 +26,50 @@ class Registr extends Component {
         this.setState({[name]: value});
     }
 
-    submitHandler = (evt) => {
+    clearAlert(duration) {
+        setTimeout(()=> {
+            this.setState({
+                error: null
+            });
+        }, duration);
+    }
+
+    submitHandler = async (evt) => {
         evt.preventDefault();
         const { displayName ,email, password, confirmPassword } = this.state;
         if(password !== confirmPassword) {
-            const newErr = {
-                text: 'Password not equels',
-                style: 'error'
-            };
             this.setState({
-                error: newErr
-            });
-
-            setTimeout(() => {
-                this.setState({
-                    error: null
-                });
-            }, 2500);
+                error: {
+                    text: 'Password not equels',
+                    style: 'error'
+                }
+            }, this.clearAlert(2000));
+            return;
         }
 
-        this.setState({
-            email: '',
-            password: ''
-        })
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
+
+            await createUserDocument(user, { displayName });
+
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            })
+        } catch (error) {
+            this.setState({
+                error: {
+                    text: error.message,
+                    style: 'error'
+                }
+            }, this.clearAlert(2000));
+        }
+        
     }
 
     render() {
@@ -60,8 +82,8 @@ class Registr extends Component {
                 <form onSubmit={this.submitHandler}>
                     <FormInput label='Name' changeHandler={this.inputChangeHandler} type="text" name="displayName" value={displayName} required />
                     <FormInput label='Email' changeHandler={this.inputChangeHandler} type="email" name="email" value={email} required />
-                    <FormInput label='Password' changeHandler={this.inputChangeHandler} type="password" name="password" value={password} required />
-                    <FormInput label='Confirm password' changeHandler={this.inputChangeHandler} type="password" name="confirmPassword" value={confirmPassword} required />
+                    <FormInput label='Password' changeHandler={this.inputChangeHandler} type="password" minLength="6" name="password" value={password} required />
+                    <FormInput label='Confirm password' changeHandler={this.inputChangeHandler} type="password" minLength="6" name="confirmPassword" value={confirmPassword} required />
                     {error ? <Alert {...error} /> : null}
                     <div className='button-group'>
                         <ButtonCustom type='submit'>Sing Up</ButtonCustom>
