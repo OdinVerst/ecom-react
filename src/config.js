896d7e281c74e17293ef8712ea2cddf0155abd95
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2020-08-12 18:07:22 +0300

    Fix to heroku

---
+++ src/config.js
@@ -0,0 +1,13 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyBIl0axAyNJoIHvxksx7JT5XNp54-7xa8Y",
+    authDomain: "crwn-bd-9aa73.firebaseapp.com",
+    databaseURL: "https://crwn-bd-9aa73.firebaseio.com",
+    projectId: "crwn-bd-9aa73",
+    storageBucket: "crwn-bd-9aa73.appspot.com",
+    messagingSenderId: "363903037447",
+    appId: "1:363903037447:web:58ec30d6b51d74b1637aa2"
+  }
+};
+
+export default config;
